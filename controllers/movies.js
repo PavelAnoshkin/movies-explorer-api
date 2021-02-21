044e22e89f69a70b1754c60871df2fa2f876b2f4
Pavel Anoshkin <monitoring.tk-ekat@yandex.ru>
2021-02-21 15:48:49 +0500

    feat: Проверять наличие карточки перед добавлением

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,11 +2,13 @@ const Movie = require('../models/movies');
 
 const NotFoundError = require('../errors/not-found-err');
 const BadRequestError = require('../errors/bad-request-err');
+const ConflictError = require('../errors/conflict-err');
 
 const {
     movieCreateErrMessage,
     movieNotFoundErrMessage,
     movieDeleteMessage,
+    movieCreateConflictErrMessage,
 } = require('../utils/constaints');
 
 module.exports.getMovies = (req, res, next) => {
@@ -31,29 +33,40 @@ module.exports.createMovie = (req, res, next) => {
         thumbnail,
     } = req.body;
 
-    Movie.create({
+    Movie.findOne({
         movieId,
-        country,
-        director,
-        duration,
-        year,
-        description,
-        image,
-        trailer,
-        nameRU,
-        nameEN,
-        thumbnail,
         owner: req.user._id,
     })
-        .then((newMovie) => Movie.findById(newMovie._id)
-                .populate('owner')
-                .then((foundMovie) => res.send({ data: foundMovie })))
-        .catch((err) => {
-            if (err._message === 'movie validation failed') {
-                throw new BadRequestError(movieCreateErrMessage);
+        .then((movie) => {
+            if (movie) {
+                throw new ConflictError(movieCreateConflictErrMessage);
             }
 
-            return next(err);
+            return Movie.create({
+                movieId,
+                country,
+                director,
+                duration,
+                year,
+                description,
+                image,
+                trailer,
+                nameRU,
+                nameEN,
+                thumbnail,
+                owner: req.user._id,
+            })
+                .then((newMovie) => Movie.findById(newMovie._id)
+                        .populate('owner')
+                        .then((foundMovie) => res.send({ data: foundMovie })))
+                .catch((err) => {
+                    if (err._message === 'movie validation failed') {
+                        throw new BadRequestError(movieCreateErrMessage);
+                    }
+
+                    return next(err);
+                })
+                .catch(next);
         })
         .catch(next);
 };
